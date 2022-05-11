598fe889a700239c58ee6316016e4c6f4a1d264c
EunJae <dldmswo1209@naver.com>
2022-05-12 00:26:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/outstagram/InstaSplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.outstagram
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstaSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insta_splash)
+
+        val sharedPreferences = getSharedPreferences("user_info", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("token", "empty")
+        when(token){
+            "empty" -> { // 로그인이 되어 있지 않은 경우
+                startActivity(Intent(this, InstaLoginActivity::class.java))
+            }
+            else -> { //로그인이 되어있는 경우
+                startActivity(Intent(this, InstaMainActivity::class.java))
+            }
+        }
+    }
+}
